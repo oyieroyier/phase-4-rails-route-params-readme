675557aa7bef693b3388ddc3062c6c1dd2fbd7d6
Bob Oyier <boboyier92@gmail.com>
2023-03-14 08:09:54 +0300

    Done

--- app/controllers/cheeses_controller.rb
+++ app/controllers/cheeses_controller.rb
@@ -1,8 +1,13 @@
 class CheesesController < ApplicationController
-
   def index
     cheeses = Cheese.all
     render json: cheeses
   end
 
+  def show
+    # Find a cheese using n ID from URL
+		cheese = Cheese.find(params[:id])
+    # Send a JSON response using cheese object
+		render json: cheese
+  end
 end
